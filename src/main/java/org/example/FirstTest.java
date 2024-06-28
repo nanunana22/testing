d39606ae24e3e0611a6939c23a717fc3ce3b2467
ursulakohler <kohlerursula22@gmail.com>
2024-06-28 11:16:51 +0200

    addTest_when8and8thenreturn16

---
+++ src/main/java/org/example/FirstTest.java
@@ -0,0 +1,28 @@
+//Erstellen Sie ein neues Java-Projekt und implementieren Sie die folgenden Methoden und testen Sie sie:
+
+//Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
+
+//Schreiben Sie einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist.
+
+//Schreiben Sie einen Test für eine Methode, die das Produkt zweier ganzer Zahlen errechnet.
+
+//Schreiben Sie einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+
+//Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
+
+//Bitte posten Sie den Link zu einem GitHub Repo, in dem Sie diese Testfälle zusammen implementiert haben.
+//Antwort hier eingeben
+
+//Wenn Sie diese Aufgabe bereits früher gelöst haben, können Sie die Bonusaufgabe auf der nächsten Seite bearbeiten.
+
+
+
+package org.example;
+
+public class FirstTest {
+    public static void main(String[] args) {
+    }
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+}
